2ddc75cb8bd947d08fb03d5d7c48654955d1729d
JhaoDa <jhaoda@gmail.com>
2020-07-07 14:20:36 +0300

    Simplify Label contract

--- src/Contract/Label.php
+++ src/Contract/Label.php
@@ -15,16 +15,9 @@ declare(strict_types=1);
 namespace PhpAidc\LabelPrinter\Contract;
 
 use PhpAidc\LabelPrinter\Enum\Charset;
-use PhpAidc\LabelPrinter\Enum\Direction;
 
 interface Label extends Job
 {
-    public function charset(Charset $value);
-
-    public function copies(int $copies);
-
-    public function direction(Direction $value);
-
     public function add(Command $command);
 
     public function getMedia(): array;
